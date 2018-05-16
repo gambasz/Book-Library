7000c97ab556f987743c12c7d1d7184f53beb773
Rajashow Parajuli <rajashowparajuli@gmail.com>
2018-05-16 00:52:55 -0400

    Driver with the Search based on CRN

--- Rajashow/src/Driver.java
+++ Rajashow/src/Driver.java
@@ -21,6 +21,17 @@ ResultSet rs=stmt.executeQuery("select * from MCBook");
 while(rs.next())  
 System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5));  
 
+// CRN search method <<<<
+String selectSQL = "select * from MCBook WHERE CRN = ?";
+
+
+preparedStatement = con.prepareStatement(selectSQL);
+// fill the first ? with 1
+preparedStatement.setInt(1, 1);
+ResultSet rs = preparedStatement.executeQuery();
+
+while(rs.next())  
+System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5));  
 
 con.close();  
   
