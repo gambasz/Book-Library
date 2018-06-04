cd5e5aa74053d6f4a740a8635d6eae9d0cfc81a0
guidoambasz <guidoambasz@gmail.com>
2018-06-04 13:58:16 -0400

    We have the project in the Project folder, added classes for every single object (database tables) and added DBManager (is going to be used to add the info stored in the different objects into the database)

---
+++ src/com/mbox/Person.java
@@ -0,0 +1,85 @@
+package com.mbox;
+
+public class Person {
+
+    private String first_name, last_name, type;
+
+    public Person(){
+
+        first_name = "";
+        last_name = "";
+        type = "";
+
+    }
+
+    public Person(String first_name, String last_name, String type){
+
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.type = type;
+    }
+
+    public void setFirstName(String name){
+
+        if(name.isEmpty()){
+
+            throw new IllegalArgumentException("First name can't be empty");
+        } else{
+
+            this.first_name = name;
+        }
+
+    }
+
+
+    public void setLastName(String name){
+
+        if(name.isEmpty()){
+
+            throw new IllegalArgumentException("Last name can't be empty");
+        } else{
+
+            this.last_name = name;
+        }
+
+    }
+
+    public void setType(String type){
+
+        if(!type.equals("Coordinator") && !type.equals("Instructor")){
+
+            throw new IllegalArgumentException("Invalid person type: " + type);
+        }
+
+        else{
+
+            this.type = type;
+        }
+
+    }
+
+    public String getFirstName(){
+
+        return this.first_name;
+    }
+
+
+    public String getLastName(){
+
+        return this.last_name;
+    }
+
+
+    public String getType(){
+
+        return this.type;
+    }
+
+    @Override
+    public String toString(){
+
+        return "Name: " + this.first_name + " " + this.last_name + " " + "Role: " + this.type;
+    }
+
+
+}
---
+++ src/com/mbox/Publisher.java
@@ -0,0 +1,46 @@
+package com.mbox;
+
+public class Publisher {
+
+    private String title;
+    private String contact_information;
+
+    public Publisher(){
+
+        title = "";
+        contact_information = "";
+
+    }
+
+    public Publisher(String title, String contact_information){
+
+        this.title = title;
+        this.contact_information = contact_information;
+    }
+
+    public void setTitle(String title){
+
+        this.title = title;
+    }
+
+    public void setContactInformation(String contact_info){
+
+        this.contact_information = contact_info;
+    }
+
+    public String getTitle(){
+
+        return this.title;
+    }
+
+    public String getContactInformation(){
+
+        return this.contact_information;
+    }
+
+    @Override
+    public String toString(){
+
+        return "Title: " + this.title + "\n" + "Contact Info: \n" + this.contact_information;
+    }
+}
