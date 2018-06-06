389a221e8b6af865e8f3c261e09ba082a78d5f0b
Rajashow Parajuli <rajashowparajuli@gmail.com>
2018-06-05 21:50:24 -0400

    basic redesign of the gui. Datamanager method complatibity check

--- Rajashow/src/Controller.java
+++ Rajashow/src/Controller.java
@@ -1,4 +1,6 @@
 import data.Course;
+import data.Person;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -28,22 +30,37 @@ public class Controller {
     @FXML
     TableColumn<Course, Integer> crnCol;
     @FXML
-    TableColumn<Course, String> profCol;
+    TableColumn<Course, Person> profCol;
     @FXML
     TableColumn<Course, String> courseCol;
     @FXML
     TableColumn<Course, String> departCol;
     @FXML
     TableColumn<Course, ArrayList> resourceCol;
-// boos to ist
+    @FXML
+    ComboBox semesterComBox, semesterComBoxEdit;
+    @FXML
+    ComboBox yearComBox, yearComBoxEdit;
 
     @FXML
     public void initialize() {
+        initComboBoxes();
+        resourceCol.setSortable(false);
         table_data = new ArrayList<>();
         setCellValueOfColumns();
         tableTV.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         makeCellsEditable();
         init_tables();
+    }
+
+    private void initComboBoxes() {
+        semesterComBox.setItems(FXCollections.observableArrayList("Fall", "Winter", "Spring", "Summer"));
+        semesterComBoxEdit.setItems(FXCollections.observableArrayList("Fall", "Winter", "Spring", "Summer"));
+        ArrayList<String> years = new ArrayList<>();
+        for (int i = 1920; i < 2018; i++)
+            years.add("" + i);
+        yearComBox.setItems(FXCollections.observableArrayList(years));
+        yearComBoxEdit.setItems(FXCollections.observableArrayList(years));
     }
 
 
@@ -83,48 +100,7 @@ public class Controller {
 
     public void add() {
 
-        HBox mainAddPane = new HBox(2);
-        VBox labelsPane = new VBox(4);
-        VBox textFieldsPane = new VBox(4);
-        Dialog dlg = new Dialog();
-
-        ImageView icon = new ImageView(this.getClass().getResource("/media/icon.png").toString());
-        icon.setFitHeight(100);
-        icon.setFitWidth(100);
-
-        TextField crnTF = new TextField();
-        TextField profTF = new TextField();
-        TextField classTF = new TextField();
-        TextField bookTF = new TextField();
-
-        Label crnLbl = new Label("CRN: ");
-        Label profLbl = new Label("Professor's Name: ");
-        Label classLbl = new Label("data.Course' Name: ");
-        Label bookLbl = new Label("Book: ");
-
-        ButtonType addNewClass = new ButtonType("Add The New data.Course", ButtonBar.ButtonData.OK_DONE);
-
-        labelsPane.getChildren().addAll(crnLbl, profLbl, classLbl, bookLbl);
-        textFieldsPane.getChildren().addAll(crnTF, profTF, classTF, bookTF);
-        mainAddPane.getChildren().addAll(labelsPane, textFieldsPane);
-
-        dlg.setTitle("Adding A New data.Course");
-        dlg.setHeaderText("Add a New data.Course");
-
-        dlg.setGraphic(icon);
-        dlg.getDialogPane().setMinWidth(300);
-        dlg.getDialogPane().setContent(mainAddPane);
-        dlg.getDialogPane().getButtonTypes().addAll(addNewClass, ButtonType.CANCEL);
-
-
-        dlg.show();
-        dlg.setResultConverter(dialogButton -> {
-            if (dialogButton == addNewClass) {
-                System.out.print("adding the class");
-                return null;
-            }
-            return null;
-        });
+        System.out.println("Added to the db");
 
 
     }
@@ -137,37 +113,111 @@ public class Controller {
     private void init_tables() {
         ArrayList<String> books = new ArrayList<>();
         books.add("major");
-        Course temp = new Course(0, "bob", "bob", "bob", books);
-        table_data.add(temp);
+//        Course temp = new Course(0, "bob", "bob", "bob", books);
+//        table_data.add(temp);
 
         for (int i = 1; i < 10; i++) {
-            temp = new Course(i, "cat", "bob", "bob", books);
-            table_data.add(temp);
+//            table_data.add(temp);
         }
         tableTV.getItems().addAll(table_data);
 
     }
 
     private void makeCellsEditable() {
-        profCol.setCellFactory(TextFieldTableCell.forTableColumn());
         courseCol.setCellFactory(TextFieldTableCell.forTableColumn());
         departCol.setCellFactory(TextFieldTableCell.forTableColumn());
+
+
     }
 
     private void setCellValueOfColumns() {
         crnCol.setCellValueFactory(
                 new PropertyValueFactory<Course, Integer>("CRN"));
         profCol.setCellValueFactory(
-                new PropertyValueFactory<Course, String>("professor"));
+                new PropertyValueFactory<Course, Person>("professor"));
         courseCol.setCellValueFactory(
                 new PropertyValueFactory<Course, String>("name"));
         departCol.setCellValueFactory(
                 new PropertyValueFactory<Course, String>("department"));
         resourceCol.setCellValueFactory(
-                new PropertyValueFactory<Course, ArrayList>("books"));
+                new PropertyValueFactory<Course, ArrayList>("resource"));
+
+//        resourceCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Course, Button>, ObservableValue<Button>>() {
+//            final TableCell<Person, String> cell = new TableCell<Person, String>() {
+//
+//                final Button btn = new Button("Just Do It");
+//
+//                @Override
+//            public ObservableValue<Button> call(TableColumn.CellDataFeatures<Course, Button> param) {
+//                return ;
+//            }
+//        });
     }
 
     public void exit() {
         System.exit(0);
     }
+
+    public void delete() {
+    }
+
+    public void exportData() {
+    }
+
+    public void importData() {
+    }
+
+    public void modifyResources() {
+        System.out.print("needs to be implemented");
+    }
+
+
+    public void selectProf() {
+        VBox mainAddPane = new VBox(2);
+
+        Dialog dlg = new Dialog();
+
+        ImageView icon = new ImageView(this.getClass().getResource("/media/icon.png").toString());
+        icon.setFitHeight(100);
+        icon.setFitWidth(100);
+        ComboBox listOfCurrentProf = new ComboBox();
+        TextField fNameTF = new TextField();
+        TextField lNameTF = new TextField();
+        ComboBox tpyeBox = new ComboBox();
+
+        Label fNameLbl = new Label("Professor's First Name: ");
+        Label lNameLbl = new Label("Professor's Last Name: ");
+        Label typeLbl = new Label("Type: ");
+        Label currentCBoxLbl = new Label("Current Professor templates: ");
+
+        ButtonType assign = new ButtonType("Assign", ButtonBar.ButtonData.OK_DONE);
+
+
+        mainAddPane.getChildren().addAll(
+                new HBox(currentCBoxLbl, listOfCurrentProf),
+                new HBox(fNameLbl, fNameTF),
+                new HBox(lNameLbl, lNameTF),
+                new HBox(typeLbl, tpyeBox));
+        mainAddPane.setSpacing(20);
+
+        dlg.setTitle("Assigning Professor");
+        dlg.setHeaderText("Assigning Professor");
+
+        dlg.setGraphic(icon);
+        dlg.getDialogPane().setMinWidth(300);
+        dlg.getDialogPane().setContent(mainAddPane);
+        dlg.getDialogPane().getButtonTypes().addAll(assign, ButtonType.CANCEL);
+
+
+        dlg.show();
+        dlg.setResultConverter(dialogButton -> {
+            if (dialogButton == assign) {
+                System.out.print("Assign new prof");
+                return null;
+            }
+            return null;
+        });
+
+    }
+
 }
--- Rajashow/src/Main.java
+++ Rajashow/src/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
         primaryStage.setTitle("MC Books");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
 
