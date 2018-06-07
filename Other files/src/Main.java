6f8e0e1931006a16b4fe60cac67701559d733c7e
mmohades <mmohades@hotmail.com>
2018-06-07 11:22:01 -0400

    Put files in root

---
+++ Other files/src/Main.java
@@ -0,0 +1,22 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("text/gui.fxml"));
+        primaryStage.setTitle("MC Books");
+        primaryStage.setScene(new Scene(root, 1200, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ Other files/src/data/Publisher.java
@@ -0,0 +1,71 @@
+package data;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Publisher {
+    // NAME , ID, contact, DESCRIPTION,
+    final String NAME, DESCRIPTION;
+    final int ID;
+    private String contacts;
+
+    public Publisher(String NAME, String DESCRIPTION, int ID) {
+        this.NAME = NAME;
+        this.DESCRIPTION = DESCRIPTION;
+        this.ID = ID;
+    }
+
+    public Publisher(String NAME, String DESCRIPTION, int ID, String contacts) {
+        this.NAME = NAME;
+        this.DESCRIPTION = DESCRIPTION;
+        this.ID = ID;
+        this.contacts = contacts;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public String getDESCRIPTION() {
+        return DESCRIPTION;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(String contacts) {
+        this.contacts = contacts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Publisher publisher = (Publisher) o;
+        return ID == publisher.ID &&
+                Objects.equals(NAME, publisher.NAME) &&
+                Objects.equals(DESCRIPTION, publisher.DESCRIPTION);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(NAME, DESCRIPTION, ID);
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "NAME='" + NAME + '\'' +
+                ", DESCRIPTION='" + DESCRIPTION + '\'' +
+                ", ID=" + ID +
+                ", contacts=" + contacts +
+                '}';
+    }
+
+}
