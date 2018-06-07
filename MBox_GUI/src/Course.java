aca7ec97f2d9bd5d69b3ca8a623149482219fedb
Rajashow Parajuli <rajashowparajuli@gmail.com>
2018-06-07 13:12:19 -0400

    simple course db connettion v1

---
+++ MBox_GUI/src/Course.java
@@ -0,0 +1,64 @@
+import javafx.beans.property.SimpleStringProperty;
+
+public class Course {
+
+
+    private SimpleStringProperty Semester,title,resource,intstutor;
+    public Course() {
+    Semester=title=resource=intstutor=new SimpleStringProperty("Needs to be assigned");
+    }
+    public Course(String semester, String title, String resource, String intstutor) {
+        Semester =new SimpleStringProperty( semester);
+        this.title =new SimpleStringProperty( title);
+        this.resource =new SimpleStringProperty( resource);
+        this.intstutor =new SimpleStringProperty( intstutor);
+    }
+
+    public String getSemester() {
+        return Semester.get();
+    }
+
+    public SimpleStringProperty semesterProperty() {
+        return Semester;
+    }
+
+    public void setSemester(String semester) {
+        this.Semester.set(semester);
+    }
+
+    public String getTitle() {
+        return title.get();
+    }
+
+    public SimpleStringProperty titleProperty() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title.set(title);
+    }
+
+    public String getResource() {
+        return resource.get();
+    }
+
+    public SimpleStringProperty resourceProperty() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource.set(resource);
+    }
+
+    public String getIntstutor() {
+        return intstutor.get();
+    }
+
+    public SimpleStringProperty intstutorProperty() {
+        return intstutor;
+    }
+
+    public void setIntstutor(String intstutor) {
+        this.intstutor.set(intstutor);
+    }
+}
