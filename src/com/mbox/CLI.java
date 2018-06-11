ba556cf3343ad09684a31dda827f259c13891027
Khanh Hoang <thoang43@montgomerycollege.edu>
2018-06-11 12:24:42 -0400

    Changed alittle in CLI, I will work on search method and make the instruction clearer later

--- src/com/mbox/CLI.java
+++ src/com/mbox/CLI.java
@@ -22,7 +22,7 @@ public class CLI {
             String input = scan.nextLine();
 
             String[] values = input.split(" -");
-            if(input == null){
+            if (input == null) {
                 continue;
             }
 
@@ -42,91 +42,29 @@ public class CLI {
                 case "insert": {
 
                     String table = values[1];
-                    switch (table) {
-                        case "PERSON":
-                        case "Person":
-                        case "person": {
-                            try {
-
-                                Person p = new Person(values[2],values[3],values[4]);
-                                DB.executeNoReturnQuery(DB.qiPerson(p));
-                                System.out.println("Added Person");
-                            }catch(Exception e){
-                                System.out.println("It must be Firstname Lastname Type");
-                            }
-                            break;
-                        }
-                        case "Resource":
-                        case "RESOURCE":
-                        case "resource": {
-                            try {
-                                Resource r = new Resource(values[2], values[3], values[4], values[5], Integer.valueOf(values[6]),
-                                        Integer.valueOf(values[7]), values[8]);
-                                DB.executeNoReturnQuery(DB.qiResource(r));
-                                System.out.println("Added Resource");
-                            }catch(Exception e){
-                                System.out.println("It must be type title author isbn total current description");
-                            }
-                            break;
-                        }
-                        case "Course":
-                        case "COURSE":
-                        case "course": {
-                            try {
-                                Course c = new Course(values[2], values[3], values[4], values[5]);
-                                DB.executeNoReturnQuery(DB.qiCourse(c));
-                                System.out.println("Added Course");
-                            }catch(Exception e){
-                                System.out.println("It must be title crn description department");
-                            }
-                            break;
-                        }
-                        default:
-                            System.out.println("There is no table like that.");
-                    }
+                    insertTable(table,values,DB);
                     break;
                 }
+
                 case "SHOW":
                 case "show":
                 case "Show": {
                     String table = values[1];
-                    switch (table) {
-                        case "PERSON":
-                        case "Person":
-                        case "person": {
-
-                            DBManager.getTablePerson();
-                            break;
-                        }
-
-                        case "Resource":
-                        case "RESOURCE":
-                        case "resource": {
-                            DBManager.getTableResources();
-                            break;
-                        }
-
-                        case "Publisher":
-                        case "PUBLISHER":
-                        case "publisher": {
-                            DBManager.getTablePublishers();
-                            break;
-                        }
-
-                        default:
-                            System.out.println("There is no table like that.");
-                    }
+                    showTable(table);
                     break;
                 }
+
                 default:
                     System.out.println("Enter Help to know more");
             }
-
         }
-
-
-
     }
+
+
+
+
+
+
     public static void showHelp(){
         System.out.println("-------Instruction--------");
         System.out.println("To add to person: INSERT -Person -<Firstname> -<Lastname> -<Type>");
@@ -134,6 +72,80 @@ public class CLI {
                 "-<currentAmount> -<Description>");
         System.out.println("1. To add to publisher: INSERT -Publisher -<Title> -<contactInfo> -<description>");
         System.out.println("2. To see data in table: SHOW -<Name of table> ");
+    }
+
+    public static void showTable(String table) {
+        switch (table) {
+            case "PERSON":
+            case "Person":
+            case "person": {
+
+                DBManager.getTablePerson();
+                break;
+            }
+
+            case "Resource":
+            case "RESOURCE":
+            case "resource": {
+                DBManager.getTableResources();
+                break;
+            }
+
+            case "Publisher":
+            case "PUBLISHER":
+            case "publisher": {
+                DBManager.getTablePublishers();
+                break;
+            }
+            default: System.out.println("There is no table like that.");
+        }
+    }
+
+
+    public static void insertTable(String table, String[] values,DBManager DB){
+        switch(table){
+            case"PERSON":
+            case"Person":
+            case"person":{
+                try{
+
+                     Person p=new Person(values[2],values[3],values[4]);
+                     DB.executeNoReturnQuery(DB.qiPerson(p));
+                     System.out.println("Added Person");
+                }catch(Exception e){
+                    System.out.println("It must be Firstname Lastname Type");
+                 }
+                break;
+            }
+
+            case"Resource":
+            case"RESOURCE":
+            case"resource":{
+                try{
+                    Resource r=new Resource(values[2],values[3],values[4],values[5],Integer.valueOf(values[6]),
+                    Integer.valueOf(values[7]),values[8]);
+                    DB.executeNoReturnQuery(DB.qiResource(r));
+                    System.out.println("Added Resource");
+                }catch(Exception e){
+                    System.out.println("It must be type title author isbn total current description");
+            }
+                break;
+        }
+
+            case"Course":
+            case"COURSE":
+            case"course":{
+                try{
+                    Course c=new Course(values[2],values[3],values[4],values[5]);
+                    DB.executeNoReturnQuery(DB.qiCourse(c));
+                    System.out.println("Added Course");
+                }catch(Exception e){
+                    System.out.println("It must be title crn description department");
+            }
+                break;
+        }
+            default: System.out.println("There is no table like that.");
+        }
     }
 }
 
