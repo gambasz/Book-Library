a33a452cffd75e239aebe387b5f1ca0117859ce3
Rajashow Parajuli <rajashowparajuli@gmail.com>
2018-06-13 19:27:38 -0400

    Semester Enumeration Structure

---
+++ MBox_GUI/src/data/Semester.java
@@ -0,0 +1,20 @@
+package data;
+
+public enum Semester {
+    FALL("FALL"), SUMMER("SUMMER"), SPRING("SPRING"), WINTER("WINTER");
+
+    Semester(String semester) {
+        this.semester = semester.toUpperCase();
+    }
+
+    private String semester;
+
+    public String semester() {
+        return semester;
+    }
+
+    @Override
+    public String toString() {
+        return semester;
+    }
+}
