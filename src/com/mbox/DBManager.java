1265fb13d4c27323dabda1383d55889667c2c995
mmohades <mmohades@hotmail.com>
2018-06-18 14:40:25 -0400

    Adding return evertything functions from prbackend

--- src/com/mbox/DBManager.java
+++ src/com/mbox/DBManager.java
@@ -1130,6 +1130,91 @@ public class DBManager {
     }
 
 
+    public static int[] getCourseIdsBySemesterID(int id){
+
+        int i = 0;
+        String query = String.format("SELECT * FROM RELATION_SEMESTER_COURSE WHERE SEMESTERID=%d", id);
+
+        try{
+
+            Statement st = DBManager.conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            while(rs.next()){
+                i++;
+            }
+
+            int[] ids = new int[i];
+
+            rs = st.executeQuery(query);
+            i = 0;
+            while(rs.next()){
+
+                ids[i] = rs.getInt(1);
+                i++;
+            }
+
+            return ids;
+
+
+        }catch(SQLException e){
+
+            System.out.println("Something went wrong");
+
+        }
+
+        return null;
+    }
+
+
+    public static ArrayList<Course> returnEverything(int semesterid) {
+
+        int arr[] = getCourseIdsBySemesterID(semesterid);
+
+        ArrayList<Course> hugeshit = new ArrayList<>();
+
+        for(int i = 0; i < arr.length; i++){
+
+            Course[] tmp = DBManager.relationalReadByCourseID(arr[i]);
+
+            for(int j = 0; j < tmp.length; j++) {
+
+                hugeshit.add(tmp[j]);
+
+            }
+        }
+
+        return hugeshit;
+    }
+
+    // Returns id needs to return title;
+    public static Course[] getCourseTitlesByID(int[] ids){
+
+        Course[] c;
+        try{
+
+            Statement st = DBManager.conn.createStatement();
+            c = new Course[ids.length];
+
+            for(int i = 0; i < ids.length; i++){
+
+                ResultSet rs = st.executeQuery(String.format("SELECT * FROM COURSECT WHERE ID=%d", ids[i]));
+                while(rs.next()){
+
+                    c[i] = new Course(rs.getString(2) + rs.getString(3));
+                }
+
+                return c;
+
+            }
+
+
+        }catch(SQLException e){
+
+        }
+        return null;
+    }
+
 }
 
 
--- src/com/mbox/Main.java
+++ src/com/mbox/Main.java
@@ -2,6 +2,7 @@ package com.mbox;
 
 import javax.swing.plaf.nimbus.State;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -9,73 +10,10 @@ public class Main {
 
         DBManager.openConnection();
 
-        Course[] mamali = DBManager.relationalReadByCourseID(11);
+        //Course[] mamali = DBManager.relationalReadByCourseID(11);
+        ArrayList<Course> lopm = DBManager.returnEverything(57);
 
         DBManager.closeConnection();
         }
 
-        public static int[] getCourseIdsBySemesterID(int id){
-
-            int i = 0;
-            String query = String.format("SELECT * FROM RELATION_SEMESTER_COURSE WHERE SEMESTERID=%d", id);
-
-            try{
-
-                Statement st = DBManager.conn.createStatement();
-                ResultSet rs = st.executeQuery(query);
-
-                while(rs.next()){
-                    i++;
-                }
-
-                int[] ids = new int[i];
-
-                rs = st.executeQuery(query);
-                i = 0;
-                while(rs.next()){
-
-                    ids[i] = rs.getInt(1);
-                    i++;
-                }
-
-                return ids;
-
-
-            }catch(SQLException e){
-
-                System.out.println("Something went wrong");
-
-            }
-
-            return null;
-        }
-
-
-        // Returns id needs to return title;
-        public static Course[] getCourseTitlesByID(int[] ids){
-
-        Course[] c;
-        try{
-
-            Statement st = DBManager.conn.createStatement();
-            c = new Course[ids.length];
-
-            for(int i = 0; i < ids.length; i++){
-
-                ResultSet rs = st.executeQuery(String.format("SELECT * FROM COURSECT WHERE ID=%d", ids[i]));
-                while(rs.next()){
-
-                    c[i] = new Course(rs.getString(2) + rs.getString(3));
-                }
-
-                return c;
-
-            }
-
-
-        }catch(SQLException e){
-
-        }
-            return null;
-        }
     }
