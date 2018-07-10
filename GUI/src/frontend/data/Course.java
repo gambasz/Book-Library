5f8dcacf861c1bf75bd48650d758027a573793c2
Rajashow Parajuli <rajashowparajuli@gmail.com>
2018-07-10 11:58:04 -0400

    Testing the data for tree tables and forces styles on the add view

--- GUI/src/frontend/data/Course.java
+++ GUI/src/frontend/data/Course.java
@@ -1,5 +1,6 @@
 package frontend.data;
 
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
