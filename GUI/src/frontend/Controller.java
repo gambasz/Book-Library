17653885602c3effbd707cb2cbf0d4cedd68c717
mmohades <mmohades@hotmail.com>
2018-07-17 14:52:35 -0400

    Add button check for correct formatting of course title

--- GUI/src/frontend/Controller.java
+++ GUI/src/frontend/Controller.java
@@ -604,44 +604,55 @@ public class Controller {
 
     public void add() {
 
-        boolean professorChanged = false, courseChanged = false, resourceChanged = false;
-        Course tempCour = new Course();
-        if (selectedCourse == null) {
-            if(courseInfoDepart.getText().trim().isEmpty()  || courseInfoDescrip.getText().trim().isEmpty() || courseInfoTitle.getText().trim().isEmpty() ||
-                profInfoFName.getText().trim().isEmpty()|| profInfoLName.getText().trim().isEmpty() || profInfoType.getSelectionModel().getSelectedItem() == null ||
-                resourceTable.getItems() == null){
-                showError("Error","Missing info","You need to fulfill all sections");
-                return;
-            }
-            selectedPerson = new Person();
-            tempCour.setID(0);
-            selectedCourse = tempCour;
-            ArrayList<Resource> resArr = new ArrayList<>(resourceTable.getItems());
-            selectedCourse.setResource(resArr);
-            }
+        String tempCourseTitle = courseInfoTitle.getText().replaceAll("\\s", "");
+        System.out.println(tempCourseTitle);
+        String[] cSplit = tempCourseTitle.split("(?<=\\D)(?=\\d)");
 
-        if(selectedCourse!=null) {
-            selectedPerson = new Person(selectedPerson);
-            ArrayList<Resource> tempRes = selectedCourse.getResource();
+        if (cSplit.length !=2){
 
-            tempCour = new Course(
-                    selectedCourse.getID(),
-                    selectedCourse.getID(),
-                    Integer.parseInt(yearComBoxEdit.getSelectionModel().getSelectedItem().toString()),
-                    semesterComBoxEdit.getSelectionModel().getSelectedItem().toString(),
-                    courseInfoTitle.getText(),
-                    courseInfoDepart.getText(),
-                    selectedPerson,
-                    courseInfoDescrip.getText(),
-                    tempRes);
-
-            professorChanged = tempCour.getProfessor().getFirstName() != profInfoFName.getText() ||
-                    tempCour.getProfessor().getLastName() != profInfoLName.getText();
-            courseChanged = tempCour.getTitle() != courseInfoTitle.getText() || tempCour.getDescription() != courseInfoDepart.getText() ||
-                    tempCour.getDepartment() != courseInfoDescrip.getText();
+            showError("Course title format is Wrong",
+                    "Check the course title format and make sure it's following the correct format.",
+                    "Correct format examples --> CMSC 100, MATH 181 ");
         }
 
+        else {
 
+            boolean professorChanged = false, courseChanged = false, resourceChanged = false;
+            Course tempCour = new Course();
+            if (selectedCourse == null) {
+                if (courseInfoDepart.getText().trim().isEmpty() || courseInfoDescrip.getText().trim().isEmpty() || courseInfoTitle.getText().trim().isEmpty() ||
+                        profInfoFName.getText().trim().isEmpty() || profInfoLName.getText().trim().isEmpty() || profInfoType.getSelectionModel().getSelectedItem() == null ||
+                        resourceTable.getItems() == null) {
+                    showError("Error", "Missing info", "You need to fulfill all sections");
+                    return;
+                }
+                selectedPerson = new Person();
+                tempCour.setID(0);
+                selectedCourse = tempCour;
+                ArrayList<Resource> resArr = new ArrayList<>(resourceTable.getItems());
+                selectedCourse.setResource(resArr);
+            }
+
+            if (selectedCourse != null) {
+                selectedPerson = new Person(selectedPerson);
+                ArrayList<Resource> tempRes = selectedCourse.getResource();
+
+                tempCour = new Course(
+                        selectedCourse.getID(),
+                        selectedCourse.getID(),
+                        Integer.parseInt(yearComBoxEdit.getSelectionModel().getSelectedItem().toString()),
+                        semesterComBoxEdit.getSelectionModel().getSelectedItem().toString(),
+                        courseInfoTitle.getText(),
+                        courseInfoDepart.getText(),
+                        selectedPerson,
+                        courseInfoDescrip.getText(),
+                        tempRes);
+
+                professorChanged = tempCour.getProfessor().getFirstName() != profInfoFName.getText() ||
+                        tempCour.getProfessor().getLastName() != profInfoLName.getText();
+                courseChanged = tempCour.getTitle() != courseInfoTitle.getText() || tempCour.getDescription() != courseInfoDepart.getText() ||
+                        tempCour.getDepartment() != courseInfoDescrip.getText();
+            }
 
 
             if (professorChanged) {
@@ -677,10 +688,8 @@ public class Controller {
                 if (tempCour.getYEAR() == Integer.parseInt(yearComBox.getSelectionModel().getSelectedItem().toString())) {
                     courseList.add(tempCour);
                 }
-
-
             }
-
+        }
             updateCourseTable();
         }
 
