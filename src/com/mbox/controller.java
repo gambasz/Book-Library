96fb32aa378b9692843eb46a3331fe583508a453
mmohades <mmohades@hotmail.com>
2018-07-19 19:08:10 -0400

    getAlledition method done in backendController

--- src/com/mbox/controller.java
+++ src/com/mbox/controller.java
@@ -1,5 +1,9 @@
 package com.mbox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class controller {
 
     public static boolean isInteger(String s) {
@@ -12,5 +16,13 @@ public class controller {
         }
         // only got here if we didn't return false
         return true;
+    }
+
+    public static List<String> getAllEdition(){
+        String temp = "First;Second;Third;Fourth;Fifth;Sixth;Seventh;Eighth;Ninth;Tenth;" +
+                "Eleventh;Twelfth;Thirteenth;Fourteenth;Fifteenth";
+        String[] tempArray = temp.split(";");
+
+        return Arrays.asList(tempArray);
     }
 }
