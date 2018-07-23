2faaf6a0b13b25bd46a187f5d9800f064fae3c7c
Rajashow Parajuli <rajashowparajuli@gmail.com>
2018-07-23 14:02:22 -0400

    prompt text and spacing ?

--- GUI/src/frontend/Controller.java
+++ GUI/src/frontend/Controller.java
@@ -613,19 +613,19 @@ public class Controller {
 
 
     public void filterTableBasedOnSemesterNYear() {
-        updateCourseTable();
-        Integer year = yearComBox.getSelectionModel().getSelectedItem();
-        if (year != null) {
-            tableTV.getItems().removeIf(course -> course.getYEAR() != year.intValue());
-        }
-        Object semester = semesterComBox.getSelectionModel().getSelectedItem();
-        if (semester != null) {
-            tableTV.getItems().removeIf(course -> !course.getSEMESTER().equals(semester.toString()));
-
-        }
-        //Todo : remove code if not needed
-//        refreshTable();
 //        updateCourseTable();
+//        Integer year = yearComBox.getSelectionModel().getSelectedItem();
+//        if (year != null) {
+//            tableTV.getItems().removeIf(course -> course.getYEAR() != year.intValue());
+//        }
+//        Object semester = semesterComBox.getSelectionModel().getSelectedItem();
+//        if (semester != null) {
+//            tableTV.getItems().removeIf(course -> !course.getSEMESTER().equals(semester.toString()));
+//
+//        }
+        //Todo : remove code if not needed
+        refreshTable();
+        updateCourseTable();
 
     }
 
